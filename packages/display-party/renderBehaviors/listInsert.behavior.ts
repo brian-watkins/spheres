8e1e272bda4f726443e586b01c1be331bf683bf7
Brian Watkins <brian.watkins@gmail.com>
2023-07-24 06:17:08 -0400

    WIP - Extra insert tests

--- packages/display-party/renderBehaviors/listInsert.behavior.ts
+++ packages/display-party/renderBehaviors/listInsert.behavior.ts
@@ -1,6 +1,8 @@
 import { makeVirtualElement, virtualNodeConfig } from "@src/vdom/virtualNode.js";
 import { behavior, effect, example, fact, step } from "esbehavior";
-import { childElement, expectChild, expectTotalChildren } from "helpers/index.js";
+import { equalTo, expect, is } from "great-expectations";
+import { selectElements } from "helpers/displayElement.js";
+import { childElement, expectChild, expectTotalChildren, statefulChildElement } from "helpers/index.js";
 import { renderContext } from "helpers/renderContext.js";
 
 export default behavior("insert items into list", [
@@ -28,12 +30,124 @@ export default behavior("insert items into list", [
       ],
       observe: [
         effect("the elements are all in the list", async () => {
-          await expectTotalChildren(5)
-          await expectChild(1)
-          await expectChild(2)
-          await expectChild(3)
-          await expectChild(4)
-          await expectChild(5)
+          const texts = await selectElements("[data-child]").map(el => el.text())
+          expect(texts, is(equalTo([
+            "child 1",
+            "child 2",
+            "child 3",
+            "child 4",
+            "child 5",
+          ])))
+        })
+      ]
+    }),
+  example(renderContext())
+    .description("insert at beginning")
+    .script({
+      suppose: [
+        fact("there is a list of elements", (context) => {
+          context.mount(makeVirtualElement("div", virtualNodeConfig(), [
+            childElement(1),
+            childElement(2),
+          ]))
+        })
+      ],
+      perform: [
+        step("some elements are added to the beginning", (context) => {
+          context.patch(makeVirtualElement("div", virtualNodeConfig(), [
+            childElement(3),
+            childElement(4),
+            childElement(5),
+            childElement(1),
+            childElement(2),
+          ]))
+        })
+      ],
+      observe: [
+        effect("the elements are all in the list", async () => {
+          const texts = await selectElements("[data-child]").map(el => el.text())
+          expect(texts, is(equalTo([
+            "child 3",
+            "child 4",
+            "child 5",
+            "child 1",
+            "child 2",
+          ])))
+        })
+      ]
+    }),
+  example(renderContext())
+    .description("insert throughout")
+    .script({
+      suppose: [
+        fact("there is a list of elements", (context) => {
+          context.mount(makeVirtualElement("div", virtualNodeConfig(), [
+            childElement(1),
+            childElement(2),
+            childElement(3),
+          ]))
+        })
+      ],
+      perform: [
+        step("some elements are added throughout", (context) => {
+          context.patch(makeVirtualElement("div", virtualNodeConfig(), [
+            childElement(1),
+            childElement(4),
+            childElement(2),
+            childElement(5),
+            childElement(3),
+          ]))
+        })
+      ],
+      observe: [
+        effect("the elements are all in the list in the right order", async () => {
+          const texts = await selectElements("[data-child]").map(el => el.text())
+          expect(texts, is(equalTo([
+            "child 1",
+            "child 4",
+            "child 2",
+            "child 5",
+            "child 3",
+          ])))
+        })
+      ]
+    }),
+  example(renderContext())
+    .description("insert new keyed elements throughout")
+    .script({
+      suppose: [
+        fact("there are some keyed elements", (context) => {
+          context.mount(makeVirtualElement("div", virtualNodeConfig(), [
+            statefulChildElement(1),
+            statefulChildElement(2),
+          ]))
+        })
+      ],
+      perform: [
+        step("new keyed elements are inserted", (context) => {
+          context.patch(makeVirtualElement("div", virtualNodeConfig(), [
+            statefulChildElement(8),
+            statefulChildElement(1),
+            statefulChildElement(3),
+            statefulChildElement(4),
+            statefulChildElement(2),
+            statefulChildElement(5),
+            statefulChildElement(6),
+          ]))
+        })
+      ],
+      observe: [
+        effect("the elements are all in the right order", async () => {
+          const texts = await selectElements("[data-stateful-child]").map(el => el.text())
+          expect(texts, is(equalTo([
+            "stateful child 8",
+            "stateful child 1",
+            "stateful child 3",
+            "stateful child 4",
+            "stateful child 2",
+            "stateful child 5",
+            "stateful child 6",
+          ])))
         })
       ]
     })

358a756fc2dd7e3f782599c144bd04077321f5b8
Brian Watkins <brian.watkins@gmail.com>
2023-09-07 06:33:39 -0400

    Add test for when remove stateful element
    - it should unsubscribe from the store query that
    drives its updates

--- packages/display-party/renderBehaviors/unsubscribe.behavior.ts
+++ packages/display-party/renderBehaviors/unsubscribe.behavior.ts
@@ -1,6 +1,6 @@
 import { behavior, effect, example, fact, step } from "esbehavior";
 import { renderContext } from "./helpers/renderContext.js";
-import { makeReactiveTextNode, makeVirtualElement, makeVirtualTextNode, virtualNodeConfig } from "@src/vdom/virtualNode.js";
+import { addAttribute, makeReactiveTextNode, makeStatefulElement, makeVirtualElement, makeVirtualTextNode, virtualNodeConfig } from "@src/vdom/virtualNode.js";
 import { Container, container } from "state-party";
 import { selectElement } from "helpers/displayElement.js";
 import { expect, is, resolvesTo } from "great-expectations";
@@ -11,6 +11,54 @@ interface UnsubscribeContext {
 }
 
 export default behavior("unsubscribe", [
+
+  example(renderContext<UnsubscribeContext>())
+    .description("remove stateful element")
+    .script({
+      suppose: [
+        fact("there is a stateful element", (context) => {
+          context.setState({
+            queryCount: 0,
+            container: container({ initialValue: "tennis" })
+          })
+          const nestedStatefulChild = makeVirtualElement("section", virtualNodeConfig(), [
+            makeStatefulElement(virtualNodeConfig(), (get) => {
+              context.state.queryCount++
+              const config = virtualNodeConfig()
+              addAttribute(config, "class", "funny")
+              return makeVirtualElement("div", config, [
+                makeVirtualTextNode(`Your favorite sport is: ${get(context.state.container)}`)
+              ])
+            })
+          ])
+          context.mount(makeVirtualElement("div", virtualNodeConfig(), [
+            nestedStatefulChild
+          ]))
+        })
+      ],
+      observe: [
+        effect("the element was displayed", async () => {
+          await expect(selectElement(".funny").text(), resolvesTo("Your favorite sport is: tennis"))
+        }),
+        effect("the query is executed", (context) => {
+          expect(context.state.queryCount, is(1))
+        })
+      ]
+    }).andThen({
+      perform: [
+        step("the stateful element is removed", (context) => {
+          context.patch(makeVirtualElement("div", virtualNodeConfig(), []))
+        }),
+        step("the container is updated", (context) => {
+          context.writeTo(context.state.container, "running")
+        })
+      ],
+      observe: [
+        effect("the query is not executed again", (context) => {
+          expect(context.state.queryCount, is(1))
+        })
+      ]
+    }),
 
   example(renderContext<UnsubscribeContext>())
     .description("remove reactive text")

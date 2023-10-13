98d13a50f692d75034fd7a56140cb08ae5406b06
Brian Watkins <brian.watkins@gmail.com>
2023-10-13 06:17:52 -0400

    [Refactor] Combine some local state

--- examples/src/circles/state.ts
+++ examples/src/circles/state.ts
@@ -11,30 +11,8 @@ export interface Circle {
   selected: boolean
 }
 
-interface AddCircleMessage {
-  type: "add-circle"
-  center: Coordinate
-  selected: boolean
-}
-
-export function addCircleAt(center: Coordinate, selected: boolean = false): AddCircleMessage {
-  return {
-    type: "add-circle",
-    center,
-    selected
-  }
-}
-
-// type CirclesMessage = AddCircleMessage
-
 export const circleData: Container<Array<CircleContainer>> = container({
   initialValue: [],
-  // reducer: (message, current: Array<CircleContainer>) => {
-    // switch (message.type) {
-      // case "add-circle":
-        // return [ ...current, circleContainer(message.center, message.selected) ]
-    // }
-  // }
 })
 
 export interface CircleSelectionMessage {
@@ -86,38 +64,6 @@ function circleContainer(center: Coordinate, selected: boolean): CircleContainer
     }
   })
 }
-
-// We could have a container that represents when a circle is capable of
-// being adjusted. Although what this really represents is the state of the
-// UI -- since it's whether we are opening the window to adjust the diameter
-// that this gets set
-
-export const adjustableCircle = container<CircleContainer | undefined>({
-  initialValue: undefined
-})
-
-
-// For undo, redo
-// one approach would be to have a container with a list of messages
-// and then the circleData is a value that depends on this list of messages
-// when it changes then it basically rehydrates from this list.
-// But there needs to be a pointer to an index in the list as well. And the
-// value would need to depend on that as well. When we undo, we decrement
-// the pointer. When we redo we increase it. And when we make a significant
-// action, we add it at the current index and delete the remaining.
-// This seems like a pretty literal approach. Is there another way?
-
-// The problem with the above is that adding circles is easy, but adjusting the
-// diameter occurs on a different state object. So you'd somehow need to
-// know which circle needs to be adjusted etc.
-
-// I guess there are a few cases.
-// 1. Adjusting radius -- this means that the circle must exist and have a previous radius
-// undo in this case means the circle goes back to the previous radius
-// 2. Adding a circle -- this means that the circle did not exist and undo means remove
-// this circle.
-
-// Ok so maybe we have a list of actions like:
 
 interface Action {
   execute: StoreMessage<any>
@@ -204,18 +150,3 @@ export const canUndo = value({
 export const canRedo = value({
   query: (get) => get(currentAction) !== get(actions).length - 1
 })
-
-// And then when you hit undo you get the Action at the current index and call undo() to get
-// a message which you process -- this would all be in a selection.
-// And to redo, you increase the index, get the action at that index and the return the message
-// from the redo function.
-// To do a new action, you have a selection that adds at the current index and deletes subsequent
-// actions. And then also returns the message from do()
-// So to add a circle you would create something like an action
-// where do is to send an AddCircle message -- where it refers to a CircleContainer
-// and undo is to remove the CircleContainer from the array.
-// And to adjust diameter:
-// do sends a updateradius message to the circleContainer
-// and undo sends a updateReadius message to the previous radius
-// So hopefully we create the CircleContainer once, and even if we undo it, we
-// still have a reference to it if we have to redo and even adjust the radius again
--- examples/src/circles/view.ts
+++ examples/src/circles/view.ts
@@ -1,6 +1,6 @@
 import { View, htmlView, svgView } from "display-party";
-import { GetState, batch, write, run, container, store } from "state-party";
-import { CircleContainer, addCircleSelection, adjustRadius, adjustRadiusSelection, adjustableCircle, canRedo, canUndo, circleData, deselectCircle, redoSelection, selectCircle, undoSelection } from "./state";
+import { GetState, batch, write, run, store, container } from "state-party";
+import { CircleContainer, addCircleSelection, adjustRadius, adjustRadiusSelection, canRedo, canUndo, circleData, deselectCircle, redoSelection, selectCircle, undoSelection } from "./state";
 import { useValue } from "../helpers/helpers";
 
 export default function circles(): View {
@@ -24,7 +24,6 @@ export default function circles(): View {
                 .class("disabled:text-slate-200 text-slate-500")
                 .disabled(get => !get(canRedo))
                 .on("click", () => store(redoSelection))
-
               children
                 .textNode("Redo")
             })
@@ -37,10 +36,8 @@ export default function circles(): View {
             .class("bg-slate-300 rounded")
             .on("click", (evt) => {
               const mouseEvent = (evt as unknown as MouseEvent)
-              // return write(circleData, addCircleAt({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, true))
               return store(addCircleSelection, { x: mouseEvent.offsetX, y: mouseEvent.offsetY })
             })
-
           children
             .andThen(circleViews)
         })
@@ -75,11 +72,12 @@ function circleView(circleContainer: CircleContainer) {
           .on("mouseout", () => write(circleContainer, deselectCircle()))
           .on("click", (evt) => {
             evt.stopPropagation()
-            console.log("Adjusting circle", circleContainer.toString(), get(circleContainer).radius)
             return batch([
+              write(dialog, {
+                circle: circleContainer,
+                originalRadius: get(circleContainer).radius,
+              }),
               write(showDiameterSlider, false),
-              write(adjustableCircle, circleContainer),
-              write(originalRadius, get(circleContainer).radius),
               run(() => {
                 document.querySelector("dialog")?.showModal()
               })
@@ -89,29 +87,33 @@ function circleView(circleContainer: CircleContainer) {
   }
 }
 
-const originalRadius = container({ initialValue: 0 })
+// local state for dialog
+
+interface DialogContents {
+  circle: CircleContainer
+  originalRadius: number
+}
+
+const dialog = container<DialogContents | undefined>({
+  initialValue: undefined
+})
 
 const showDiameterSlider = container({ initialValue: false })
 
 function optionsView(get: GetState): View {
-  const adjustable = get(adjustableCircle)
+  const dialogData = get(dialog)
 
-  if (adjustable === undefined) {
+  if (dialogData === undefined) {
     return htmlView().dialog()
   }
 
-  // Note that to get the ability to close by clicking outside the
-  // dialog, we need to add a click handler to the dialog and check
-  // to make sure the target is the dialog itself.
-
-  const circle = get(adjustable)
+  const circle = get(dialogData.circle)
 
   return htmlView()
     .dialog(({ config, children }) => {
       config
         .on("click", (evt) => {
           const target = evt.target as HTMLElement
-          console.log("tag name", target.tagName)
           if (target.tagName === "DIALOG") {
             return run(() => (target as HTMLDialogElement).close())
           } else {
@@ -119,8 +121,10 @@ function optionsView(get: GetState): View {
           }
         })
         .on("close", () => {
-          console.log("Original radius", get(originalRadius))
-          return store(adjustRadiusSelection, { circle: get(adjustableCircle)!, originalRadius: get(originalRadius) })
+          return store(adjustRadiusSelection, {
+            circle: get(dialog)!.circle,
+            originalRadius: get(dialog)!.originalRadius
+          })
         })
       children
         .div(({ config, children }) => {
@@ -138,21 +142,9 @@ function optionsView(get: GetState): View {
                   .max("50")
                   .step("1")
                   .value(`${circle.radius}`)
-                  // .value(get => `${get(get(adjustableCircle)!).radius}`)
-                  .on("input", useValue(value => write(adjustable, adjustRadius(Number(value)))))
-                  // Note that this is creating a significant (undoable) event on every change
-                  // We should only send the adjust radius selection message when the
-                  // dialog box is closed
-                  // and there are two cases for that: when you hit escape and when you
-                  // click outside the dialog ... I think ...
-                  // .on("input", useValue(value => store(adjustRadiusSelection, { circle: adjustable, radius: Number(value) })))
-                  
+                  .on("input", useValue(value => write(dialogData.circle, adjustRadius(Number(value)))))
               })
           } else {
-            // Note that for some reason this was not getting the correct
-            // circle data ... maybe it wasn't getting patched correctly when the
-            // other parts got patched? And it just had a reference to the initial
-            // container?
             children
               .textNode(`Adjust Diameter of circle at (${circle.center.x}, ${circle.center.y})`)
           }

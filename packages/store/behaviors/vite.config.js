1498ffd2a1a31c7d577a9a2dc16a32cd1967f8eb
Brian Watkins <10941576+brian-watkins@users.noreply.github.com>
2023-12-13 11:46:08 +0000

    Optimize deps for store tests

--- packages/store/behaviors/vite.config.js
+++ packages/store/behaviors/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  optimizeDeps: {
+    entries: [
+      "**/*.behavior.ts"
+    ]
+  },
   plugins: [
     tsConfigPaths()
   ]

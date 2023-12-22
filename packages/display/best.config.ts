02f1d55fbfae6b6e2df88b601267769fc9f9ef96
Brian Watkins <brian.watkins@gmail.com>
2023-12-22 07:45:10 -0500

    Fix display test scripts
    - upgrade to latest best-behavior
    - remove unused test dependencies

--- packages/display/best.config.ts
+++ packages/display/best.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "best-behavior"
 
 export default defineConfig({
   behaviorGlobs: [
-    "./renderBehaviors/**/*.behavior.ts",
-    "./behaviors/**/*.behavior.ts"
+    "renderBehaviors/**/*.behavior.ts",
+    "behaviors/**/*.behavior.ts"
   ],
   browserBehaviors: {
     globs: [
-      "./renderBehaviors/**/*"
+      "renderBehaviors/**/*.behavior.ts"
     ]
   },
   viteConfig: "./behaviors/vite.config.js",

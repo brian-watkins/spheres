0bc64049ea73dfbdfe98e9a7bfcf2c1665180b8f
Brian Watkins <brian.watkins@gmail.com>
2025-11-15 06:34:35 -0500

    WIP: more comments

--- src/store/state/value.ts
+++ src/store/state/value.ts
@@ -11,11 +11,14 @@ export class Value<T> extends ValueWriter<T> {
     super(value)
   }
 
+  // Note this is only necessary if we want to allow values to be standalone state
   [getPublisher]() {
     return this
   }
 }
 
+// this is necessary if we want to drill into a State and subscribe to some
+// value inside it without also subscribing to the surrounding state
 export function valueAt<T, S>(state: State<T>, locator: (val: T) => Value<S>): WritableState<S> {
   return {
     [getPublisher](registry) {

e636045cae299aa645948090a5c6be2464ee6280
Brian Watkins <brian.watkins@gmail.com>
2026-04-28 06:40:15 -0400

    Remove unnecessary optimization from test vite config

--- behaviors/vite.config.js
+++ behaviors/vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: [
-      "vite"
-    ]
-  },
   plugins: [
     tsConfigPaths()
   ]
